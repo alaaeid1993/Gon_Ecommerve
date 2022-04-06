3851bf64201bd92875b016f8edde31df017e1018
Alaa Eid <70823641+alaaeid1993@users.noreply.github.com>
2022-04-06 03:18:40 +0200

    Add project files.

---
+++ Gon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
